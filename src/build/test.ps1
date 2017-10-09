b00234c2d4d5356ac3e23de63b886d454b68d128
Piotr Kudrel <piotr.kudrel@deneblab.com>
2017-10-09 22:38:30 +0200

    WIP

--- src/build/test.ps1
+++ src/build/test.ps1
@@ -1,3 +1,7 @@
 & git rev-parse --show-toplevel
 & git log --max-count=1 --pretty=format:%cI HEAD
 & git rev-parse --abbrev-ref HEAD
+& git symbolic-ref --short -q HEAD
+& git for-each-ref refs/tags --sort=-taggerdate --format='%(refname:short)' --count=1
+& git rev-parse --show-toplevel
+
