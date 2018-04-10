73f1b3cda2e511a14db6d8f96c2d2f46217ea038
Piotr Kudrel <piotr.kudrel@deneblab.com>
2018-04-10 10:37:42 +0200

    Update version to 1.0.5

--- src/build/bl.ps1
+++ src/build/bl.ps1
@@ -10,7 +10,7 @@ param(
 	$scriptFile = (Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) ".build.ps1"),
 	[parameter(Mandatory=$false)] [int] $major = 1,
 	[parameter(Mandatory=$false)] [int] $minor = 0,
-	[parameter(Mandatory=$false)] [int] $patch = 4,
+	[parameter(Mandatory=$false)] [int] $patch = 5,
 	$buildCounter = 0,
 	$psGitVersionStrategy = "standard"
 )
